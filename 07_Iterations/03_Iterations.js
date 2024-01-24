2a1c3b0df6e4585b4e230dcec3f7563c866016fa
Shivanjali More <shivanjalimore035@gmail.com>
2024-01-24 19:53:52 +0530

    Update 03_Iterations.js

--- 07_Iterations/03_Iterations.js
+++ 07_Iterations/03_Iterations.js
@@ -1,11 +1,33 @@
-// for of loop
+// ********************** for of loop *************************
 
 // for of statement loops through the values of an iterable object.
-
 // It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists, and more:
 
-const arr = [1,2,3,4,5]
-for(const num of arr)
+// const arr = [1,2,3,4,5]
+// for(const num of arr)
+// {
+//     console.log(num);
+// }
+
+
+// ************************** map ****************************
+
+// Key and value pair
+// map() method loops through each object in the given array
+// returns an array containing key-value pairs as its data
+
+const map = new Map()
+map.set('IN',"India");
+map.set("USA","United states of America");
+map.set("Fr","France");
+map.set("IN","India");
+console.log(map);
+
+// for (const key of map)
+// {
+//     console.log(key);
+// }
+for (const [key,value] of map)
 {
-    console.log(num);
+    console.log(key,": ",value);
 }
