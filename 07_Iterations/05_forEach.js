d43655fe48e0e57f10e0aaadf5f890bc14534215
Shivanjali More <shivanjalimore035@gmail.com>
2024-01-24 19:53:46 +0530

    Create 05_forEach.js

---
+++ 07_Iterations/05_forEach.js
@@ -0,0 +1,43 @@
+//*********************** for each **************************** 
+
+// => forEach loop does't return any value => undefined
+const random = ["ABC","DEF","GHI","JKL"];
+
+// random.forEach( function (paramter){
+//    console.log(paramter);
+// })
+
+
+// random.forEach((parameter1) => {
+//    console.log(parameter1);
+// })
+
+
+// random.forEach( (item,index,random) => {
+//     console.log(item,index,random);
+// })
+
+//******************  for each in array of Objects  ********************
+const myCoding = [
+    {
+        key1: "value1"
+    },
+    {
+        key2: "value2"
+    },
+    {
+        key3: "value3"
+    },
+    {
+        key4: "value4"
+    }
+]
+
+// myCoding.forEach(element => {
+//     console.log(element);
+// });
+
+
+ myCoding.forEach(element => {
+    console.log(element);
+});
