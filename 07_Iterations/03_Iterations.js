bb28f03a14fff952a020daffa5c99a8044e0577c
Shivanjali More <shivanjalimore035@gmail.com>
2024-01-24 12:13:14 +0530

    Create 03_Iterations.js

---
+++ 07_Iterations/03_Iterations.js
@@ -0,0 +1,11 @@
+// for of loop
+
+// for of statement loops through the values of an iterable object.
+
+// It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists, and more:
+
+const arr = [1,2,3,4,5]
+for(const num of arr)
+{
+    console.log(num);
+}
